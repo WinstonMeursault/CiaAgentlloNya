5028d0f408f3665a727811c65c39f4231845a986
WinstonMeursault <86593997@qq.com>
2026-03-21 13:12:24 +0800

    Support for streaming response.

--- bot.py
+++ bot.py
@@ -1,9 +1,12 @@
-from yaml import safe_load as yamlSafeLoad
+import asyncio
+from time import time
 
+from yaml import safe_load as yamlSafeLoad
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram.error import RetryAfter
 
-from neko import askNeko
+from neko import askNeko, askNekoStream
 
 with open('./config/config.yaml', 'r') as yamlConfig:
     botConfig = yamlSafeLoad(yamlConfig)['TelegramBot']
@@ -18,17 +21,52 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await context.bot.send_message(chat_id=update.effective_chat.id,text=botReplyTemplate['help'])
 
-async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:    
-    await update.message.reply_text(askNeko(update.message.text))
+async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=askNeko(update.message.text))
+    
+async def chatStream(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    steamID = int(round(time() * 1000))
+    accumulatedText = ""
+    buffer = ""
+    flushInterval = 0.05
+    bufferMaxSize = 30
+    lastFlush = asyncio.get_event_loop().time()
 
+    async for delta in askNekoStream(update.message.text):
+        buffer += delta
+        now = asyncio.get_event_loop().time()
 
+        if now - lastFlush >= flushInterval or len(buffer) > bufferMaxSize:
+            accumulatedText += buffer
+            while True:
+                try:
+                    await context.bot.send_message_draft(chat_id=update.effective_chat.id, draft_id=steamID, text=accumulatedText)
+                    break
+                except RetryAfter as e:
+                    await asyncio.sleep(e.retry_after)
+
+            buffer = ""
+            lastFlush = now
+
+    if buffer:
+        accumulatedText += buffer
+        while True:
+            try:
+                await context.bot.send_message_draft(chat_id=update.effective_chat.id, draft_id=steamID, text=accumulatedText)
+                break
+            except RetryAfter as e:
+                await asyncio.sleep(e.retry_after)
+    
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=accumulatedText)
+    
 def main() -> None:
     application = Application.builder().token(botConfig['Token']).build()
     
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help))
     
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, chat))
+    # application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, chat))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, chatStream))
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
--- neko.py
+++ neko.py
@@ -1,6 +1,9 @@
-from yaml import safe_load as yamlSafeLoad
+from typing import Optional, AsyncGenerator
+from json import loads as jsonLoads, JSONDecodeError
 
-import requests
+import aiohttp
+from yaml import safe_load as yamlSafeLoad
+from requests import post as requestsPost
 
 with open('./config/config.yaml', 'r') as yamlConfig:
     nekomimiConfig = yamlSafeLoad(yamlConfig)['Nekomimi']
@@ -11,8 +14,17 @@ with open('./config/inf.yaml', 'r') as yamlInf:
 if nekomimiConfig['DefaultLanguage'] == 'CN':
     with open('./config/prompt_CN.yaml', 'r') as yamlPrompt:
         nekomimiPrompt = yamlSafeLoad(yamlPrompt)
+        
+postURL = inf['API Provider URL'][nekomimiConfig['API Provider']]
+postHeaders = {
+    "Authorization": "Bearer " + nekomimiConfig['Token'],
+    "Content-Type": "application/json"
+}
 
-def extractText(resp):
+def generatePrompt(request: str) -> str:
+    return nekomimiPrompt['askNeko'] + request
+
+def parseText(resp: dict) -> str:
     texts = []
     for item in resp.get("output", []):
         if item.get("type") == "message":
@@ -21,15 +33,52 @@ def extractText(resp):
                     texts.append(content.get("text"))
     return "\n".join(texts)
 
+def parseTextSteam(line: str) -> Optional[str]:
+    line = line.strip()
+
+    if not line.startswith("data:"):
+        return None
+
+    payload = line[5:].strip()
+
+    if payload == "[DONE]":
+        return None
+
+    try:
+        obj = jsonLoads(payload)
+    except JSONDecodeError:
+        return None
+
+    if obj.get("type") == "response.output_text.delta":
+        return obj.get("delta")
+
+    return None
+
 def askNeko(request: str) -> str:
-    url = inf['API Provider URL'][nekomimiConfig['API Provider']]
-    headers = {
-    "Authorization": "Bearer " + nekomimiConfig['Token'],
-    "Content-Type": "application/json"
-    }
     data = {
     "model": nekomimiConfig['Model'],
-    "input": nekomimiPrompt['askNeko'] + request
+    "input": generatePrompt(request)
     }
-    res = requests.post(url, json=data, headers=headers)
-    return extractText(res.json())
+    
+    res = requestsPost(postURL, json=data, headers=postHeaders)
+    return parseText(res.json())
+
+async def askNekoStream(request: str) -> AsyncGenerator[str, None]:
+    data = {
+        "model": nekomimiConfig['Model'],
+        "input": generatePrompt(request),
+        "stream": True
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(postURL, json=data, headers=postHeaders) as resp:
+            async for raw_line in resp.content:
+                line = raw_line.decode("utf-8").strip()
+                if not line:
+                    continue
+
+                delta = parseTextSteam(line)
+                
+                if delta:
+                    yield delta
+                    
